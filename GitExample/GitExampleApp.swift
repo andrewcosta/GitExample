3719cfb90287c44d4788aa7cb8cb7a89eabb6c55
Andrew Costa <andrewdiniz@gmail.com>
2022-10-11 15:38:20 -0300

    Initial Commit

---
+++ GitExample/GitExampleApp.swift
@@ -0,0 +1,17 @@
+//
+//  GitExampleApp.swift
+//  GitExample
+//
+//  Created by Andrew Costa on 11/10/22.
+//
+
+import SwiftUI
+
+@main
+struct GitExampleApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
